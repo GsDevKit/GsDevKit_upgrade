9ff4abf999112f1b0052867a0d296154e30d0f4c
Dale Henrichs <dale.henrichs@gemtalksystems.com>
2019-04-19 11:33:25 -0700

    install.sh successfully runs and creates update_solo snapshot ...

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,7 +3,7 @@
 #	install stash and Rowan, then create a stash solo extent to be used for 
 #	running the deployment script
 #
-set -ex 
+set -e 
 
 cd $GS_HOME/shared/repos
 
@@ -23,14 +23,15 @@ fi
 
 $GS_HOME/shared/repos/stash/bin/install.sh $stoneName $version
 
+# install GsDevKit_upgrade into the solo stone
 projectSpecUrl="file:$GS_HOME/shared/repos/GsDevKit_upgrade/rowan/specs/GsDevKit_upgrade.ston"
-$GS_HOME/shared/repos/stash/scripts/rowan.st --install="$projectSpecUrl"
+$GS_HOME/shared/repos/stash/scripts/rowan.st --install="$projectSpecUrl" -- $stoneName -l
 
-$GS_HOME/shared/repos/stash/scripts/snapshot.st solo.dbf
-
+# create a solo extent and shut down the stone ... only the extent is needed
+$GS_HOME/shared/repos/stash/scripts/snapshot.st solo.dbf -- $stoneName -l
 stopStone -b $stoneName
 
-echo "Solo script extent is ready. Use the follow env vars for easy access to extent from scripts:"
+echo "Solo script extent is ready. Define the follow env vars for easy access to extent from scripts:"
 echo "	export GEMSTONE_SCRIPT_SOLO_EXTENT=\"\$GS_HOME/server/stones/$stoneName/snapshots/extent0.solo.dbf\""
 echo "	export GEMSTONE_SOLO_SCRIPT_ARGS="$stoneName -lq -C GEM_SOLO_EXTENT=\$GEMSTONE_SCRIPT_SOLO_EXTENT;\""
 

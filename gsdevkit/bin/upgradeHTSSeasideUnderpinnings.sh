32e373dc61b4d0ff35c45972d635624911245535
Richard Sargent <rsargent@5x5.on.ca>
2019-06-20 17:46:04 -0400

    Work with .topazini only in ~, not random directories

--- gsdevkit/bin/upgradeHTSSeasideUnderpinnings.sh
+++ gsdevkit/bin/upgradeHTSSeasideUnderpinnings.sh
@@ -10,12 +10,12 @@ die () {
 stone=$1
 password=$2
 
-if [ -e .topazini ]
+if [ -e ~/.topazini ]
 then
-	mv .topazini .topazini.upgradeHTSSeasideUnderpinnings
+	mv ~/.topazini ~/.topazini.upgradeHTSSeasideUnderpinnings
 	echo Moved existing .topazini to .topazini.upgradeHTSSeasideUnderpinnings before creating new one
 fi
-cat <<EOF >.topazini
+cat <<EOF >~/.topazini
 set user Dev password $password gemstone $stone
 EOF
 
@@ -36,7 +36,7 @@ EOF
 
 # ========================================================================================
 # Run the GsDevKit_upgrade
-cat <<EOF >.topazini
+cat <<EOF >~/.topazini
 set user Dev password swordfish gemstone $stone
 EOF
 
@@ -83,50 +83,48 @@ export GS_HOME=/var/lib/files/gs
 export upgradeLogDir=/home/gs/data/upgradeLogDir
 export upgradeDir="$GS_HOME/shared/repos/GsDevKit_upgrade/gemstone"
 
-pushd "${upgradeLogDir}" >& /dev/null
+pushd "${upgradeLogDir}" /dev/null
   echo "STARTING GsdevKit_upgrade upgradeImage "
 	echo "1. Starting installGsDevKit_upgrade"
 #	startTopaz $GEMSTONE_NAME -lq < $GS_HOME/shared/repos/GsDevKit_upgrade/bin/installGsDevKit_upgrade
-	topaz -l <$GS_HOME/shared/repos/GsDevKit_upgrade/bin/installGsDevKit_upgrade >$upgradeLogDir/installGsDevKit_upgrade.log
+	topaz -l <$GS_HOME/shared/repos/GsDevKit_upgrade/bin/installGsDevKit_upgrade 2>&1 >$upgradeLogDir/installGsDevKit_upgrade.log
 	[ $? -eq 0 ] || exitAfterError "installGsDevKit_upgrade"
 	echo "1. Finished installGsDevKit_upgrade"
 
 	echo "2. Starting prepareGsDevKitImage"
 #	startTopaz $GEMSTONE_NAME -lq < $GS_HOME/shared/repos/GsDevKit_upgrade/bin/prepareGsDevKitImage
-	topaz -l <$GS_HOME/shared/repos/GsDevKit_upgrade/bin/prepareGsDevKitImage >$upgradeLogDir/prepareGsDevKitImage.log
+	topaz -l <$GS_HOME/shared/repos/GsDevKit_upgrade/bin/prepareGsDevKitImage 2>&1 >$upgradeLogDir/prepareGsDevKitImage.log
 	[ $? -eq 0 ] || exitAfterError "prepareGsDevKitImage"
 	echo "2. Finished prepareGsDevKitImage"
 
 	echo "3. Starting prepareGsDevKitImage_pragma_user"
 #	startTopaz $GEMSTONE_NAME -lq < $GS_HOME/shared/repos/GsDevKit_upgrade/bin/prepareGsDevKitImage_pragma_user
-	topaz -l <$GS_HOME/shared/repos/GsDevKit_upgrade/bin/prepareGsDevKitImage_pragma_user >$upgradeLogDir/prepareGsDevKitImage_pragma_user.log
+	topaz -l <$GS_HOME/shared/repos/GsDevKit_upgrade/bin/prepareGsDevKitImage_pragma_user 2>&1 >$upgradeLogDir/prepareGsDevKitImage_pragma_user.log
 	[ $? -eq 0 ] || exitAfterError "prepareGsDevKitImage_pragma_user"
 	echo "3. Finished prepareGsDevKitImage_pragma_user"
 
 	echo "4. Starting prepareGsDevKitImage_pragma_SystemUser"
 #	startTopaz $GEMSTONE_NAME -lq < $GS_HOME/shared/repos/GsDevKit_upgrade/bin/prepareGsDevKitImage_pragma_SystemUser
-	topaz -l <$GS_HOME/shared/repos/GsDevKit_upgrade/bin/prepareGsDevKitImage_pragma_SystemUser >$upgradeLogDir/prepareGsDevKitImage_pragma_SystemUser.log
+	topaz -l <$GS_HOME/shared/repos/GsDevKit_upgrade/bin/prepareGsDevKitImage_pragma_SystemUser 2>&1 >$upgradeLogDir/prepareGsDevKitImage_pragma_SystemUser.log
 	[ $? -eq 0 ] || exitAfterError "prepareGsDevKitImage_pragma_SystemUser"
 	echo "4. Finished prepareGsDevKitImage_pragma_SystemUser"
 
 	echo "5. Starting prepareGsDevKitImage_user"
 #	startTopaz $GEMSTONE_NAME -lq -T 500MB < $GS_HOME/shared/repos/GsDevKit_upgrade/bin/prepareGsDevKitImage_user
-	topaz -l -T 500MB <$GS_HOME/shared/repos/GsDevKit_upgrade/bin/prepareGsDevKitImage_user >$upgradeLogDir/prepareGsDevKitImage_user.log
+	topaz -l -T 500MB <$GS_HOME/shared/repos/GsDevKit_upgrade/bin/prepareGsDevKitImage_user 2>&1 >$upgradeLogDir/prepareGsDevKitImage_user.log
 	[ $? -eq 0 ] || exitAfterError "prepareGsDevKitImage_user"
 	echo "5. Finished prepareGsDevKitImage_user"
 
-echo "Quitting after finishing Dale's stuff and before loading the application code (Grease, GLASS, Seaside, etc.)"
-exit
 	echo "6. Starting loadApplication_user (Metacello, Grease, GLASS, Seaside, etc.)"
-	topaz -l <$GS_HOME/shared/repos/GsDevKit_upgrade/bin/loadApplication_user >$upgradeLogDir/loadApplication_user.log
+	topaz -l <$GS_HOME/shared/repos/GsDevKit_upgrade/bin/loadApplication_user 2>&1 >$upgradeLogDir/loadApplication_user.log
 	[ $? -eq 0 ] || exitAfterError "loadApplication_user"
 	echo "6. Finished loadApplication_user"
 
-	echo "7. Starting reloadSessionMethods_user"
-echo "##### Not implemented yet #####"
-#	topaz -l <$GS_HOME/shared/repos/GsDevKit_upgrade/bin/reloadSessionMethods_user >$upgradeLogDir/reloadSessionMethods_user.log
-	[ $? -eq 0 ] || exitAfterError "reloadSessionMethods_user"
-	echo "6. Finished reloadSessionMethods_user"
+#	echo "7. Starting reloadSessionMethods_user"
+#echo "##### Not implemented yet #####"
+##	topaz -l <$GS_HOME/shared/repos/GsDevKit_upgrade/bin/reloadSessionMethods_user 2>&1 >$upgradeLogDir/reloadSessionMethods_user.log
+#	[ $? -eq 0 ] || exitAfterError "reloadSessionMethods_user"
+#	echo "6. Finished reloadSessionMethods_user"
 popd
 
 
@@ -215,10 +213,10 @@ EOF
 
 
 cleanup_topazini () {
-rm .topazini
-if [ -e .topazini.upgradeHTSSeasideUnderpinnings ]
+rm ~/.topazini
+if [ -e ~/.topazini.upgradeHTSSeasideUnderpinnings ]
 then
-	mv .topazini.upgradeHTSSeasideUnderpinnings .topazini
+	mv ~/.topazini.upgradeHTSSeasideUnderpinnings ~/.topazini
 	echo Restored existing .topazini from .topazini.upgradeHTSSeasideUnderpinnings
 fi
 }
